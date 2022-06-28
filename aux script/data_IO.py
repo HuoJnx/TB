8870b030837d7e86998f2befb50db97fe6b104a4
HuoJnx <40828030+HuoJnx@users.noreply.github.com>
2022-06-28 11:11:10 +0800

    Create data_IO.py

---
+++ aux script/data_IO.py
@@ -0,0 +1,34 @@
+#-------------------------- read --------------------------
+otu_sep="\t"
+meta_sep=","
+taxa_sep=","
+
+def read_otu(otu_path,sep=otu_sep):
+    df_otu=pd.read_csv(otu_path,index_col=0,sep=sep)
+    return(df_otu)
+
+def read_meta(meta_path,sep=meta_sep):
+    df_meta=pd.read_csv(meta_path,index_col=0,sep=sep)
+    return(df_meta)
+
+def read_taxa(taxa_path,sep=taxa_sep):
+    df_taxa=pd.read_csv(taxa_path,index_col=0,sep=sep)
+    return(df_taxa)
+
+def read_data(otu_path,meta_path,taxa_path):
+    df_otu=read_otu(otu_path)
+    df_meta=read_meta(meta_path)
+    df_taxa=read_taxa(taxa_path)
+    return(df_otu,df_meta,df_taxa)
+
+#-------------------------- write --------------------------
+
+
+def write_otu(df_otu,otu_path,sep=otu_sep):
+    df_otu.write(otu_path,sep=otu_sep)
+
+def write_meta(df_meta,meta_path,sep=meta_sep):
+    df_meta.write(meta_path,sep=meta_sep)
+
+def write_taxa(df_taxa,taxa_path,sep=taxa_sep):
+    df_taxa.write(taxa_path,sep=taxa_sep)
