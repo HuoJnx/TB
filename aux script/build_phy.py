f15bba6015684e2b9a6354ca69373977146a40b4
HuoJnx <40828030+HuoJnx@users.noreply.github.com>
2022-07-01 05:10:27 +0800

    Update

--- aux script/build_phy.py
+++ aux script/build_phy.py
@@ -1,7 +1,33 @@
 ## help print
 def pretty_print(t):
     print(t+"\n")
-
+    
+## help pair the index of otu_table and taxa_table
+def pair_otu_taxa(df_otu, df_taxa,prompt=True):
+    """
+    The phyloseq require the df_otu and df_taxa should have the same index, so should pair them.
+    
+    Before using the function, should confirm that all the taxaid in df_otu is includes in df_taxa, otherwise will give unexpected outputs.
+    
+    The function will first use the index in df_otu to reindex the df_taxa, so you should confrim that the index in df_otu is taxid.
+    
+    The function will then replace the np.nan and pd.NA by None, which is accepted by p2r function in rpy2.
+    """
+    # ------------------------------ prompt ------------------------------
+    if prompt:
+        pretty_print(
+            """The original length of otu_table is {},
+            that of taxa_table is {},
+            match the index of taxa_table based on otu_table automaticlly.""".format(len(df_otu), len(df_taxa))
+        )
+        
+    # ------------------------------ match index ------------------------------
+    df_taxa = df_taxa.reindex(index=df_otu.index)
+    
+    # ------------------------------ replace the np.nan and pd.NA by None (p2r only accept None, not np.nan or pd.NA) ------------------------------
+    #### df_taxa = df_taxa.replace(to_replace=[np.nan,pd.NA], value=None)
+    df_taxa=df_taxa.mask(df_taxa.isna(),other=None)
+    return df_taxa
 ## R wrapper for building a phyloseq object from otu_df, meta_df and taxa_df
 
 def build_phy(df, dfg, dft=NA, taxa_are_rows=True, prompt=True):
@@ -39,35 +65,3 @@ def build_phy(df, dfg, dft=NA, taxa_are_rows=True, prompt=True):
 
     phy_obj = my_phy_sub.build_phy(df.values, df.index, dfg, dft, taxa_are_rows)
     return phy_obj
-
-
-
-
-def pair_otu_taxa(df_otu, df_taxa,prompt=True):
-    """
-    The phyloseq require the df_otu and df_taxa should have the same index, so should pair them.
-    
-    Before using the function, should confirm that all the taxaid in df_otu is includes in df_taxa, otherwise will give unexpected outputs.
-    
-    The function will first use the index in df_otu to reindex the df_taxa, so you should confrim that the index in df_otu is taxid.
-    
-    The function will then replace the np.nan and pd.NA by None, which is accepted by p2r function in rpy2.
-    """
-    # ------------------------------ prompt ------------------------------
-    if prompt:
-        pretty_print(
-            "The original length of otu_table is {}, that of taxa_table is {}, match the index of taxa_table based on otu_table automaticlly.".format(
-                len(df_otu), len(df_taxa)
-            )
-        )
-        
-    # ------------------------------ match index ------------------------------
-    df_taxa = df_taxa.reindex(index=df_otu.index)
-    
-    # ------------------------------ replace the np.nan and pd.NA by None (p2r only accept None, not np.nan or pd.NA) ------------------------------
-    df_taxa = df_taxa.replace(to_replace=[np.nan,pd.NA], value=None)
-    #### df_taxa=df_taxa.mask(df_taxa.isna(),other=None)
-    return df_taxa
-
-
-
--- aux script/convert_rank.py
+++ aux script/convert_rank.py
@@ -51,41 +51,46 @@ def convert_rank(df_otu,df_taxa,df_taxa_id,select_rank,sort_index="median"):
     df_taxa_info=df_taxa_info.drop_duplicates()
     df_merge=df_merge.drop(columns=["#OTU ID"])
     ## convert the original taxa to wanted taxa (just groupby and sum the abs abundance)
-    df_coverted=df_merge.groupby("taxa_name",as_index=True).sum()
+    df_converted=df_merge.groupby("taxa_name",as_index=True).sum()
     
     ## sort the df_otu by descending
-    df_coverted=sort_by_row(df_coverted,sort_index=sort_index, ascending=ascending)
+    df_converted=sort_by_row(df_converted,sort_index=sort_index, ascending=False)
     
     ## merget again with the taxa info
-    df_coverted=df_coverted.merge(df_taxa_info,left_on="taxa_name",right_on="taxa_name",how="left")
+    df_converted=df_converted.merge(df_taxa_info,left_on="taxa_name",right_on="taxa_name",how="left")
     
-    # ------------------------------- process the corresponding df_taxa -------------------------------
+    # ------------------------------- process the corresponding taxa table -------------------------------
 
     ## drop the col after the select col
     index_select_rank=df_taxa.columns.get_loc(select_rank)
-    df_taxa_coverted=df_taxa.iloc[:,0:index_select_rank+1]
+    df_taxa_converted=df_taxa.iloc[:,0:index_select_rank+1]
     
     ## drop duplicates
-    df_taxa_coverted=df_taxa_coverted.drop_duplicates(subset=[select_rank])
+    df_taxa_converted=df_taxa_converted.drop_duplicates(subset=[select_rank])
     
     ## match the index of otu table to df_taxa
-    df_taxa_coverted=df_coverted.reindex(columns=["taxa_name","#OTU ID"]).merge(df_taxa_coverted,left_on="taxa_name",right_on=select_rank,how="left")
+    df_taxa_converted=df_converted.reindex(columns=["taxa_name","#OTU ID"]).merge(df_taxa_converted,left_on="taxa_name",right_on=select_rank,how="left")
     
-    ## final polishment for df_taxa
-    df_taxa_coverted=df_taxa_coverted.set_index("#OTU ID")
-    df_taxa_coverted=df_taxa_coverted.drop(columns=["taxa_name"])
-    
-    # ------------------------------- process the otu table -------------------------------
-    ## final polishment for df_otu
-    df_coverted=df_coverted.set_index("#OTU ID")
-    df_coverted=df_coverted.drop(columns="taxa_name")
-    
-    # ------------------------------ get the relative abundance --------------------------------------
+    # ------------------------------- final polishment for taxa and otu table  -------------------------------
+    ## taxa table
+    #### df_taxa_converted use #OTU ID as index
+    df_taxa_converted2=df_taxa_converted.copy()
+    df_taxa_converted=df_taxa_converted.set_index("#OTU ID")
+    df_taxa_converted=df_taxa_converted.drop(columns=["taxa_name"])
+    #### df_taxa_converted2 use taxa_name as index 
+    df_taxa_converted2=df_taxa_converted2.set_index("taxa_name")
+    df_taxa_converted2=df_taxa_converted2.drop(columns=["#OTU ID"])
 
-    ## can get a rel otu table too
-    df_coverted_rel=df_coverted/df_coverted.sum()
+    ## otu table 
+    #### df_converted use #OTU ID as index
+    df_converted2=df_converted.copy()
+    df_converted=df_converted.set_index("#OTU ID")
+    df_converted=df_converted.drop(columns="taxa_name")
+    #### df_converted2 use taxa_name as index
+    df_converted2=df_converted2.set_index("taxa_name")
+    df_converted2=df_converted2.drop(columns="#OTU ID")
     
     # ------------------------------ output -----------------------------------
     ## return
     pretty_print("Successfully convert to '{}' rank.".format(select_rank))
-    return(df_coverted,df_coverted_rel,df_taxa_coverted)
+    return(df_converted,df_converted2,df_taxa_converted,df_taxa_converted2)
